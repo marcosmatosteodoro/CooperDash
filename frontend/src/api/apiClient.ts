1db02bc57589ac9ef34808b8d8950265c7d691b0
Marcos Paulo Matos Teodoro <90528223+marcosmatosteodoro@users.noreply.github.com>
2025-08-23 18:52:09 -0300

    refactor: update Dockerfile and docker-compose for improved environment configuration and build process

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://nginx/api';
+const apiUrl =
+  typeof window === 'undefined'
+    ? 'http://nginx/api' // SSR (rodando no container)
+    : process.env.NEXT_PUBLIC_API_URL || 'http://localhost/api';
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: apiUrl,
