029a7aa1d350571df79d8fe3f869490f441d7d85
Marcos Paulo Matos Teodoro <90528223+marcosmatosteodoro@users.noreply.github.com>
2025-08-23 10:06:22 -0300

    feat: configure Nginx as a reverse proxy and update API URL in apiClient

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://nginx/api';
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: apiUrl,
