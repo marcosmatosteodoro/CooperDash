3832e6ad27af94e43279f212289ec1afacc901a4
Marcos Paulo Matos Teodoro <90528223+marcosmatosteodoro@users.noreply.github.com>
2025-08-23 23:39:02 -0300

    refactor: add .dockerignore and update Dockerfile.frontend and docker-compose.yml for improved environment handling

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,9 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const apiUrl =
-  typeof window === 'undefined'
-    ? 'http://nginx/api' // SSR (rodando no container)
-    : process.env.NEXT_PUBLIC_API_URL || 'http://localhost/api';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost/api';
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: apiUrl,
