d9080595bbec887e49345abe9c51d51b051fdbd2
Marcos Paulo Matos Teodoro <90528223+marcosmatosteodoro@users.noreply.github.com>
2025-08-28 08:49:41 -0300

    feat: implement Votacao component with data fetching, layout setup, and delete functionality

---
+++ frontend/src/app/votacoes/[id]/page.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { useParams } from 'next/navigation'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/store';
+import { fetchVotacao, deleteVotacao } from '@/store/slices/votacoesSlice';
+import { fetchCooperado } from '@/store/slices/cooperadosSlice';
+import { useLayout } from '@/providers/LayoutProvider';
+import useFormatters from '@/hooks/useFormatters';
+import { useDeleteWithConfirmation } from '@/hooks/useDeleteWithConfirmation'
+import { NotFoundPage, ErrorAlert, LoadingSpinner, ShowModel } from '@/components';
+import { fetchAssembleia } from '@/store/slices/assembleiasSlice';
+
+export default function Votacao() {
+  const { id } = useParams<{ id: string }>();
+  const dispatch = useDispatch<AppDispatch>();
+  const { current, status, error } = useSelector((state: RootState) => state.votacoes);
+  const { current: cooperado } = useSelector((state: RootState) => state.cooperados);
+  const { current: assembleia } = useSelector((state: RootState) => state.assembleias);
+  const { handleDelete, deleting } = useDeleteWithConfirmation({
+    entityName: 'votacao',
+    redirectTo: '/votacoes',
+    deleteAction: (id: string) => deleteVotacao(id), 
+  });
+  const { setLayoutData } = useLayout();
+  const { formatDateTime,formatDocument, formatDate } = useFormatters();
+
+  useEffect(() => {
+    dispatch(fetchVotacao(id));
+  }, [dispatch, id]);
+  
+  useEffect(() => {
+    if(current && current.cooperado_id) {
+      dispatch(fetchCooperado(current.cooperado_id));
+      dispatch(fetchAssembleia(current.assembleia_id));
+    }
+  }, [current]);
+
+  useEffect(() => {
+    setLayoutData(prev => ({
+      ...prev,
+      breadcrumbs: [
+        { path: '/', label: 'Home' }, 
+        { path: '/votacoes', label: 'Votações' }, 
+        { path: `/votacoes/${id}`, label: 'Detalhes' }
+      ],
+      title: 'Detalhes do Votação',
+      icon: 'bi-person-badge',
+      buttons: (
+        <div className="d-flex gap-2">
+          <Link className="btn btn-primary" href={`/votacoes/${id}/editar`}>
+            <i className="bi bi-pencil-square me-2"></i>Editar
+          </Link>
+          <button 
+            onClick={() => current?.id && handleDelete(current.id)}
+            className="btn btn-danger"
+          >
+            <i className="bi bi-trash me-2"></i>Excluir
+          </button>
+          <Link className="btn btn-outline-secondary" href="/votacoes">
+            <i className="bi bi-arrow-left me-2"></i>Voltar
+          </Link>
+        </div>
+      )
+    }));
+  }, [setLayoutData, current, id]);
+
+  if (status === 'loading' || status === 'idle' || deleting) return <LoadingSpinner />;
+  if (!current) return <NotFoundPage message="Votação não encontrado" />;
+  if (error) return <ErrorAlert message={error} />;
+
+
+  return (
+    <ShowModel
+      firstColumn={{
+        title: 'Informações Básicas',
+        icon: 'bi-file-text',
+        contents: [
+          {
+            label: 'Assembleia',
+            value: assembleia?.titulo || 'Carregando...'
+          },
+          {
+            label: 'Pauta',
+            value: assembleia?.pauta || 'Carregando...'
+          },
+          {
+            label: 'Data e hora',
+            value: assembleia ? formatDateTime(assembleia.data_hora) : 'Carregando...'
+          },
+          {
+            label: 'Nome',
+            value: cooperado?.nome || 'Carregando...'
+          },
+          {
+            label: cooperado?.tipo_pessoa === 'FISICA' ? 'CPF' : 'CNPJ',
+            value: cooperado ? formatDocument(cooperado.documento, cooperado.tipo_pessoa) : 'Carregando...'
+          },
+          {
+            label: 'Tipo',
+            value: cooperado?.tipo_pessoa || 'Carregando...'
+          },
+          
+        ]
+      }}
+      secondColumn={{
+        title: 'Informações do Voto',
+        icon: 'bi-info-circle',
+        contents: [
+          {
+            label: 'Voto',
+            value: current?.voto
+          },
+          {
+            label: 'Data do voto',
+            value: current?.data_voto ? formatDate(current.data_voto) : 'Carregando...'
+          },
+          {
+            label: 'Justificativa',
+            value: current?.justificativa || '-'
+          },
+        ]
+      }}
+    />
+  );
+}
