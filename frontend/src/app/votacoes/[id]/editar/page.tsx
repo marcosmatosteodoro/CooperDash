ce15d0e5ef1845bdcf512d97d9ecc15c7a7bf500
Marcos Paulo Matos Teodoro <90528223+marcosmatosteodoro@users.noreply.github.com>
2025-08-28 09:05:44 -0300

    feat: add EditarVotacao component for editing voting details with form handling and data fetching

---
+++ frontend/src/app/votacoes/[id]/editar/page.tsx
@@ -0,0 +1,141 @@
+'use client'
+
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { useParams, useRouter } from 'next/navigation'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from '@/store';
+import { fetchVotacao } from '@/store/slices/votacoesSlice';
+import { useLayout } from '@/providers/LayoutProvider'
+import useFormatters from '@/hooks/useFormatters';
+import useVotacaoForm from '@/hooks/useVotacaoForm';
+import {Form, ErrorAlert, LoadingSpinner, NotFoundPage} from '@/components/';
+import type { FormProps } from '@/types/ui'
+
+export default function EditarVotacao() {
+  const { id } = useParams();
+  const router = useRouter();
+  const dispatch: AppDispatch = useDispatch();
+  const { current, status, error, fieldErrors } = useSelector((state: RootState) => state.votacoes);
+  const { setLayoutData } = useLayout();
+  const { formatCep } = useFormatters();
+  const { 
+    formData,
+    cooperadoOptions,
+    assembleiaOptions,
+    setFormData,
+    handleChange,
+    handleSubmitEditVotacao,
+    clearVotacaoError 
+  } = useVotacaoForm();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchVotacao(id as string));
+    }
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (current) {
+      setFormData(current);
+    }
+  }, [current, setFormData]);
+
+  useEffect(() => {
+    setLayoutData(prev => ({
+      ...prev,
+      breadcrumbs: [
+        { path: '/', label: 'Home' }, 
+        { path: '/votacoes', label: 'Votações' }, 
+        { path: `/votacoes/${id}`, label: 'Detalhes' },
+        { path: `/votacoes/${id}/editar`, label: 'Edição' }
+      ],
+      title: 'Editar Votação',
+      icon: 'bi-pencil-square',
+      buttons: (
+        <Link className="btn btn-outline-secondary" href={`/votacoes/${id}`}>
+          <i className="bi bi-arrow-left me-2"></i>Voltar
+        </Link>
+      )
+    }));
+  }, [setLayoutData, current, id]);
+
+  if (status === 'loading' || status === 'idle') return <LoadingSpinner />;
+  if (!current ) return <NotFoundPage message="Votação não encontrado" />;
+
+  const formProps: FormProps = {
+    onClick: () => router.push('/votacoes'),
+    handleSubmit: handleSubmitEditVotacao,
+    fieldErrors: fieldErrors,
+    fields: [
+      {
+        label: 'Assembleia',
+        type: 'text',
+        tag: 'select',
+        name: 'assembleia_id',
+        contentClassName: 'col-md-6',
+        value: formData.assembleia_id,
+        onChange: handleChange,
+        options: assembleiaOptions
+      },
+      {
+        label: 'Cooperado',
+        type: 'text',
+        tag: 'select',
+        name: 'cooperado_id',
+        contentClassName: 'col-md-6',
+        value: formData.cooperado_id,
+        onChange: handleChange,
+        options: cooperadoOptions
+      },
+      {
+        label: 'Data da votação',
+        type: 'text',
+        tag: 'input',
+        name: 'data_voto',
+        value: formData.data_voto,
+        contentClassName: 'col-md-6',
+        onChange: handleChange
+      },
+      {
+        label: 'Voto',
+        type: 'text',
+        tag: 'select',
+        name: 'voto',
+        contentClassName: 'col-md-6',
+        value: formData.voto,
+        onChange: handleChange,
+        options: [
+          {
+            value: "FAVOR",
+            text: "Favor"
+          },
+          {
+            value: "CONTRA",
+            text: "Contra"
+          },
+          {
+            value: "ABSTENCAO",
+            text: "Abstenção"
+          },
+        ]
+      },
+      {
+        label: 'Justificativa',
+        type: 'text',
+        tag: 'textarea',
+        name: 'justificativa',
+        value: formData.justificativa,
+        contentClassName: 'col-md-6',
+        onChange: handleChange
+      },
+    ]
+  }
+
+  return (
+    <>
+      {error && <ErrorAlert message={error} onClick={() => { clearVotacaoError() }}/>} 
+      <Form {...formProps} />
+    </>
+  );
+}
